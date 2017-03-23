bc2337c934d67dc385747a40ec3987e420110f6e
T-Farmer <terrillfarmer@gmail.com>
2017-03-23 09:46:26 -0500

    in class coding following the exercise example

---
+++ server01.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const http = require('http')
+const server = http.createServer()
+server.on('request', (req, res) => {
+    console.log('Request received for:', req.url)
+    res.end('OK')
+})
+server.listen(8080)
