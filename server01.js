6b5e190d410182fc388c639725f9d2a1396ca670
T-Farmer <terrillfarmer@gmail.com>
2017-03-23 09:57:38 -0500

    server working now to stream image

--- server01.js
+++ server01.js
@@ -4,6 +4,6 @@ const http = require('http')
 const server = http.createServer()
 server.on('request', (req, res) => {
     console.log('Request received for:', req.url)
-    res.end('OK')
+    res.end('<h1>My First Node.js Web Server</h1>')
 })
 server.listen(8080)
